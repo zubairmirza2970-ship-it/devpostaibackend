85f880f85fe3db8fcfd77d22a6205a85533ab7c5
zubair <zzubairahmed402@gmail.com>
2026-02-22 01:59:55 +0500

    Add Vercel frontend to CORS allowed origins

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:5173',
+  'https://devpost-a-ifrontend.vercel.app',
   process.env.CLIENT_URL
 ].filter(Boolean);
 
